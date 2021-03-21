0a7d0aa158c4bd7144931558b6a20419b62e6164
Kayode <kayode.nwanze@gmail.com>
2021-03-21 09:55:35 -0400

    changed URL for jsdelivr release link

--- bookmarklets/retail-tool-kit.js
+++ bookmarklets/retail-tool-kit.js
@@ -1,4 +1,5 @@
 // a bookmarklet to load script from github that can run in Lightspeed Retail
+// add javascript: before the script when you create the bookmark
 ! function () {
   const xhr = new XMLHttpRequest();
   xhr.open("GET", "https://api.github.com/repos/Kayus85/LS-Customizations/releases", true);
@@ -16,6 +17,6 @@
 function GIT_(releaseVersion) {
   console.log("backoffice");
   var script_ = document.createElement('script');
-  script_.src = `https://cdn.jsdelivr.net/gh/Kayus85/LS-Customizations@${releaseVersion}/display-account-id.js`;
+  script_.src = `https://cdn.jsdelivr.net/gh/Kayus85/LS-Customizations@${releaseVersion}/scripts/display-account-id.js`;
   document.body.appendChild(script_);
 };
